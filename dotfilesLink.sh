f02d07ad4c94b350347f631ddf3b12c2aec4011c
Flight289 <flight27182@gmail.com>
2022-07-26 05:57:56 +0900

    2

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/dein.toml ~/.vim/dein.toml
+ln -sf ~/dotfiles/dein_lazy.toml ~/.vim/dein_lazy.toml
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/tmuxline.conf ~/.tmux/tmuxline.conf
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
